12aba593cb91877b00b9298a683881f07059460d
felipe.domingues <felipe.domingues@calix.com>
2022-04-19 11:05:20 -0300

    Add messages

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const NO_RESULTS_MESSAGE =
+  "No results, please review your search or try a different one";
+export const LOADING_MESSAGE = "Loading...";
+export const ERROR_MESSAGE = "Something Bad Happened";
