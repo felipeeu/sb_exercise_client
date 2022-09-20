1cf99bcd91cc8b3ca9ef0f9eb81a61b8a7ad5335
felipe.domingues <felipe.domingues@calix.com>
2022-09-20 18:14:05 -0300

    testing changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,4 @@ const parseBirthday = (birthday) => (birthday ? birthday.split(" ")[0] : "");
 const getAge = (birthday) => (birthday ? moment(birthday).fromNow(true) : "");
 
 export { parseBirthday, getAge };
+// Testing changes in github
